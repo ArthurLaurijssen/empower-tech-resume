35829c9bdd1b9ca09d2e39173cca8e24099de533
Arthur Laurijssen <arthur@flexinect.com>
2025-02-13 19:07:15 +0100

    :feat: Greeting and icons' '

--- components/greeting-container/GreetingContainer.tsx
+++ components/greeting-container/GreetingContainer.tsx
@@ -1,18 +1,24 @@
 import { GreetingContainerProps } from "@/components/greeting-container/types";
 import React from "react";
+import { HandIcon } from "@/components/icons/hand-icon/HandIcon";
 
 export const GreetingContainer: React.FC<GreetingContainerProps> = ({
   greeting,
   className,
   ...props
-}) => {
+}: GreetingContainerProps) => {
   return (
     <div
-      className={`bg-white/80 backdrop-blur px-4 py-2 rounded-full flex items-center gap-2 ${className || ""}`}
+      className={`bg-white/80 backdrop-blur px-4 py-2 rounded-full ${className || ""}`}
       {...props}
     >
-      <span>👋</span>
-      <p>{greeting.message}</p>
+      {/* Greeting Title */}
+      <div className="flex items-center gap-2">
+        <HandIcon className="wave" />
+        <h1>{greeting.title}</h1>
+      </div>
+      {/* Greeting Message starting on md screen */}
+      <p className="hidden md:block">{greeting.message}</p>
     </div>
   );
 };
